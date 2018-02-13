98fc175201250817fcdfa25ceaf46f4878c66570
Matthew Page <matthewpagecs@gmail.com>
2018-02-13 16:37:31 -0500

    Fille db with mock data

--- audition_management/fill_db_with_mock_data.py
+++ audition_management/fill_db_with_mock_data.py
@@ -1,6 +1,7 @@
-from audition_management.models import CastingAccount, AuditionAccount, Role
+from audition_management.models import CastingAccount, Role, Tag
+from random import randint
 
-descrs = [
+descriptions = [
     'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc pulvinar urna sed dui auctor maximus quis nec orci.',
     'Integer congue nibh mattis purus viverra, nec euismod mi congue.',
     'Nunc auctor ipsum id erat laoreet euismod.',
@@ -8,19 +9,43 @@ descrs = [
     'Vestibulum at bibendum risus, quis consectetur dui. Donec finibus risus eget nibh congue suscipit.'
 ]
 
-addrs = [
+addresses = [
     '320 s grant st',
     '1600 pennsylvania av',
     '140 new montgomery st',
     '1600 Ampetheatre av'
 ]
 
+tags = [
+    'Male',
+    'Female',
+    'High pitched voice',
+    'Low pitched voice',
+    'Fast',
+    'Slow',
+    'Quick',
+    'Taggy',
+    'Good',
+    'Amazing'
+]
+
+tag_index = 0
 for i in range(0, 100):
-    Role.objects.create(
+    role = Role.objects.create(
         name='Test Role # {}'.format(i),
-        description=descrs[i % len(descrs)],
+        description=descriptions[i % len(descriptions)],
         domain=Role.DOMAIN_CHOICES[i % len(Role.DOMAIN_CHOICES)],
-        studio_address=addrs[i % len(addrs)],
+        studio_address=addresses[i % len(addresses)],
         agent=CastingAccount.objects.all()[0],
         status=i % 2
     )
+
+    for _ in range(0, randint(3, 10)):
+        Tag.objects.create(
+            name=tags[tag_index],
+            role=role,
+            agent=CastingAccount.objects.all()[0]
+        )
+        tag_index += 1
+        if tag_index >= len(tags):
+            tag_index = 0
