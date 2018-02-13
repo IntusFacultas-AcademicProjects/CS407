81d5400fbd54408158037860596460077e423f77
Matthew Page <matthewpagecs@gmail.com>
2018-02-13 16:39:16 -0500

    quick fix

--- audition_management/urls.py
+++ audition_management/urls.py
@@ -7,9 +7,8 @@ from audition_management import views
 app_name = "audition_management"
 urlpatterns = [
     url(r'^$', views.DashboardView.as_view(), name='index'),
-    url(r'^settings$', views.SettingsView.as_view(), name='settings')
+    url(r'^settings$', views.SettingsView.as_view(), name='settings'),
     url(r'^role/$', views.RoleView.as_view(), name='role'),
     url(r'^create/$', views.RoleCreationView.as_view(), name='create'),
     # url(r'^admin/', admin.site.urls),
-
 ]
